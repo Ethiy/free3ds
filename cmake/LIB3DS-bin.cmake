b3dcee98bc7e1eab4af594631c6aa4b8afba981a
bvallet <bvallet@c34c65bf-129e-48b5-8045-014bf8e59f61>
2009-11-05 19:02:10 +0000

    Ajout 3dsdump à lib3ds
    git-svn-id: https://codes-matis.ign.fr/svn/extern/lib3ds@105 c34c65bf-129e-48b5-8045-014bf8e59f61

--- cmake/LIB3DS-bin.cmake
+++ cmake/LIB3DS-bin.cmake
@@ -3,17 +3,18 @@
 # date: 23/10/2009
 
 # define source and header files and directories
-SET(3dsplay_FULL_SRCS ${LIB3DS_ROOT_DIR}/examples/3dsplay.c)
+set(3dsplay_FULL_SRCS ${LIB3DS_ROOT_DIR}/examples/3dsplay.c)
+set(3dsdump_FULL_SRCS ${LIB3DS_ROOT_DIR}/tools/3dsdump.c)
 
 include_directories(${LIB3DS_ROOT_DIR})
-SET(3dsplay_FULL_HEADERS "")
+set(3dsplay_FULL_HEADERS "")
+set(3dsdump_FULL_HEADERS "")
 
 # set definitions and dependencies
 set(3dsplay_DEFINITIONS "")
-if(WIN32)
-	set(3dsplay_DEFINITIONS ${DEFINITIONS} -DLIB3DS_EXPORTS)
-endif(WIN32)
-find_package(GLUT REQUIRED)
+set(3dsdump_DEFINITIONS "")
 set(3dsplay_DEPENDENCIES lib3ds GLUT)
+set(3dsdump_DEPENDENCIES lib3ds)
 
 __add_executable (3dsplay 3dsplay_FULL_SRCS 3dsplay_FULL_HEADERS 3dsplay_DEFINITIONS 3dsplay_DEPENDENCIES)
+__add_executable (3dsdump 3dsdump_FULL_SRCS 3dsdump_FULL_HEADERS 3dsdump_DEFINITIONS 3dsdump_DEPENDENCIES)
