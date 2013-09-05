8c920fd4abe61aaccf8111d4455dd59e1375da66
bvallet <bvallet@c34c65bf-129e-48b5-8045-014bf8e59f61>
2013-09-05 15:20:53 +0000

    Fixed a cmake bug for 3dsplay
    git-svn-id: https://codes-matis.ign.fr/svn/extern/lib3ds@295 c34c65bf-129e-48b5-8045-014bf8e59f61

--- cmake/Lib3DS-bin.cmake
+++ cmake/Lib3DS-bin.cmake
@@ -3,10 +3,10 @@
 # date: 23/10/2009
 
 # define source and header files and directories
-set(3dsplay_FULL_SRCS ${LIB3DS_ROOT_DIR}/examples/3dsplay.c)
-set(3dsdump_FULL_SRCS ${LIB3DS_ROOT_DIR}/tools/3dsdump.c)
+set(3dsplay_FULL_SRCS ${Lib3DS_ROOT_DIR}/examples/3dsplay.c)
+set(3dsdump_FULL_SRCS ${Lib3DS_ROOT_DIR}/tools/3dsdump.c)
 
-include_directories(${LIB3DS_ROOT_DIR})
+include_directories(${Lib3DS_ROOT_DIR})
 set(3dsplay_FULL_HEADERS "")
 set(3dsdump_FULL_HEADERS "")
 
@@ -16,5 +16,9 @@ set(3dsdump_DEFINITIONS "")
 set(3dsplay_DEPENDENCIES lib3ds GLUT)
 set(3dsdump_DEPENDENCIES lib3ds)
 
+FIND_PACKAGE(OpenGL REQUIRED)
+message(STATUS {OPENGL_LIBRARIES}=${OPENGL_LIBRARIES})
+
 __add_executable (3dsplay 3dsplay_FULL_SRCS 3dsplay_FULL_HEADERS 3dsplay_DEFINITIONS 3dsplay_DEPENDENCIES)
+target_link_libraries(3dsplay ${OPENGL_LIBRARIES})
 __add_executable (3dsdump 3dsdump_FULL_SRCS 3dsdump_FULL_HEADERS 3dsdump_DEFINITIONS 3dsdump_DEPENDENCIES)
