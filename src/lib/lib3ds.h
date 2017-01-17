b91b8726d3e46d88e28d868e1684e6b38bc36120
ENNAFII Oussama <oussama.ennafii@ign.fr>
2017-01-17 15:24:02 +0100

    add lib3ds.h as that includes all

---
+++ src/lib/lib3ds.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_LIB3DS__H
+#define INCLUDED_LIB3DS__H
+
+#include "atmosphere.h"
+#include "background.h"
+#include "camera.h"
+#include "chunk.h"
+#include "chunktable.h"
+#include "ease.h"
+#include "file.h"
+#include "io.h"
+#include "light.h"
+#include "material.h"
+#include "matrix.h"
+#include "mesh.h"
+#include "node.h"
+#include "quat.h"
+#include "shadow.h"
+#include "tcb.h"
+#include "tracks.h"
+#include "types.h"
+#include "vector.h"
+#include "viewport.h"
+
+#endif 
